2d9053086650eca298f74d784b6381997f94ad49
Yuri Demétrio Ferreira <yuri.ferreira@24hub.com>
2025-12-11 09:21:04 -0300

    add_trycatch_auth.service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,40 +5,50 @@ import jwt from 'jsonwebtoken';
 import { prisma } from '../utils/prisma';
 
 export async function login(email: string, password: string) {
-  const user = await prisma.user.findUnique({ where: { email } });
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
 
-  //if (!user) throw new Error('Usuário não encontrado');
-  if (!user) {
+    //if (!user) throw new Error('Usuário não encontrado');
+    if (!user) {
+      return {
+        token: "",
+        user: {},
+        msg: "Usuário não encontrado!"
+      };
+    }
+    
+    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+
+    //if (!passwordMatch) throw new Error('Senha incorreta');
+    if (!passwordMatch) {
+      return {
+        token: "",
+        user,
+        msg: "Senha incorreta!"
+      };
+    }
+
+    const token = jwt.sign(
+      { sub: user.id, role: user.role },
+      process.env.JWT_SECRET!,
+      { expiresIn: '8h' }
+    );
+
+    return {
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    };
+  } catch (error: any) {
+    console.error("Erro no login:", error.message);
     return {
       token: "",
       user: {},
-      msg: "Usuário não encontrado!"
+      msg: "Erro ao processar login: " + (error.message || "Erro desconhecido")
     };
   }
-  const passwordMatch = await bcrypt.compare(password, user.passwordHash);
-
-  //if (!passwordMatch) throw new Error('Senha incorreta');
-  if (!passwordMatch) {
-    return {
-      token: "",
-      user,
-      msg: "Senha incorreta!"
-    };
-  }
-
-  const token = jwt.sign(
-    { sub: user.id, role: user.role },
-    process.env.JWT_SECRET!,
-    { expiresIn: '8h' }
-  );
-
-  return {
-    token,
-    user: {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    },
-  };
 }
